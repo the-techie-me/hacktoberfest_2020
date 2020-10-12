1f4b0617bdd26ef0df3a1c2db5b9fa16aa85c96e
the-techie-me <72020850+the-techie-me@users.noreply.github.com>
2020-10-12 17:22:03 +0530

    Create switch_with_if_else.c

---
+++ switch_with_if_else.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+  
+int main() 
+{ 
+    int a = 2, b = 2; 
+    switch(a) 
+    { 
+    case 1: 
+        ; 
+  
+        if (b==5) 
+        { 
+        case 2: 
+            printf("Hey it is running!"); 
+        } 
+    else case 3: 
+    { 
+  
+    } 
+    } 
+} 
